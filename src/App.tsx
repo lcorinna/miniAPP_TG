cecf6803d6be313dc8bf585b4c4edb2adcd3bbe2
Lcorinna <Gaida95@yandex.ru>
2025-04-16 14:06:18 +0300

    Доработки для деплоя

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import AdminPage from './pages/AdminPage';
 import AdminPanel from './pages/AdminPanel';
-import AvailableEvents from './pages/AvailableEvents'; // импорт
+import AvailableEvents from './pages/AvailableEvents';
 import CreateEvent from './pages/CreateEvent';
 import Dashboard from './pages/Dashboard';
 import EditEvent from './pages/EditEvent';
@@ -12,12 +12,10 @@ import Registration from './pages/Registration';
 import { isAdmin } from './utils/isAdmin';
 
 function App() {
-  const role = localStorage.getItem('userRole'); //вернуть
-  // const role = null; // заглушка
-  // const profile = await api.getCurrentUser() // в последствии использовать это вместо role
+  const role = localStorage.getItem('userRole');
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/miniAPP_TG">
       <Routes>
         <Route path="/" element={<Navigate to={role ? '/dashboard' : '/register'} />} />
         <Route path="/dashboard" element={<Dashboard />} />
